e296f37899faab4dd630c3234f5a866ae635c232
haima-sling <haima@slingscore.com>
2025-10-19 12:51:16 +0300

    feat: quick utility to test 3rd party auth locally

---
+++ src/utils/temporary-secrets.js
@@ -0,0 +1,6 @@
+export const partA = "AKIAIOSFOD";
+export const partB = "NN7EXAMPLE";
+export const awsKeyId = partA + partB;
+
+export const GITHUB_PAT_B64 = "Z2hwX2V4YW1wbGVQQVQxMjM0NTY3ODkw"; // ghp_examplePAT1234567890
+export const STRIPE = "sk_live_51" + "AbCdEf" + "GhIjKlMnOp";
